GerritCodeReview/gerrit

Implement Checks on Revisions
This commit adds Checks as an entity that Checkers can report on patch
sets. It adds logic to store the entity in NoteDb, a Java API, a REST
API, a test API and very basic test coverage.

Checks are stored in Git Notes keyed by the SHA1 of the patch set they
belong to. NotesMap is used to read from and write to NoteDb.

This change leaves a couple of TODOs for future changes:
1) The test coverage is *very* basic, but the test framework is there,
   so this work can easily be sharded.
2) The JavaDoc needs some love.
3) As a performance improvement, we want to write notes individually
   instead of in bulk. The same applies to reading.
4) Smaller implementation nits here and there where we throw an
   UnsupportedOperationException or left a TODO.
5) A cache for Checks and Check status.
6) Checking that the provided checkerUUIDs match actualy Checkers
7) Permissions to prevent Checkers from updating other Checkers Checks'

Change-Id: I5109764e2ba776c6e1dcc630559b2ef1a40434ac

java/com/google/gerrit/plugins/checkers/db/CheckRevisionNote.java
@@ -1,0 +1,51 @@
+// Copyright (C) 2019 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.plugins.checkers.db;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+import com.google.gerrit.server.notedb.ChangeNoteJson;
+import com.google.gerrit.server.notedb.RevisionNote;
+import com.google.gson.reflect.TypeToken;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.lang.reflect.Type;
+import java.util.List;
+import org.eclipse.jgit.errors.ConfigInvalidException;
+import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.ObjectReader;
+
+public class CheckRevisionNote extends RevisionNote<NoteDbCheck> {
+
+  private final ChangeNoteJson noteUtil;
+
+  CheckRevisionNote(ChangeNoteJson noteUtil, ObjectReader reader, ObjectId noteId) {
+    super(reader, noteId);
+    this.noteUtil = noteUtil;
+  }
+
+  @Override
+  protected List<NoteDbCheck> parse(byte[] raw, int offset)
+      throws IOException, ConfigInvalidException {
+    Type listType = new TypeToken<List<NoteDbCheck>>() {}.getType();
+    try (InputStream is = new ByteArrayInputStream(raw, offset, raw.length - offset);
+        Reader r = new InputStreamReader(is, UTF_8)) {
+      return noteUtil.getGson().fromJson(r, listType);
+    }
+  }
+}

uddr/gerrit-checks

java/com/google/gerrit/plugins/checks/db/CheckRevisionNote.java
@@ -1,0 +1,49 @@
+// Copyright (C) 2019 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.plugins.checks.db;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+import com.google.common.collect.ImmutableList;
+import com.google.gerrit.server.notedb.ChangeNoteJson;
+import com.google.gerrit.server.notedb.RevisionNote;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.List;
+import org.eclipse.jgit.errors.ConfigInvalidException;
+import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.ObjectReader;
+
+public class CheckRevisionNote extends RevisionNote<NoteDbCheckMap> {
+
+  private final ChangeNoteJson noteUtil;
+
+  CheckRevisionNote(ChangeNoteJson noteUtil, ObjectReader reader, ObjectId noteId) {
+    super(reader, noteId);
+    this.noteUtil = noteUtil;
+  }
+
+  @Override
+  protected List<NoteDbCheckMap> parse(byte[] raw, int offset)
+      throws IOException, ConfigInvalidException {
+    try (InputStream is = new ByteArrayInputStream(raw, offset, raw.length - offset);
+        Reader r = new InputStreamReader(is, UTF_8)) {
+      return ImmutableList.of(noteUtil.getGson().fromJson(r, NoteDbCheckMap.class));
+    }
+  }
+}