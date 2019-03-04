GerritCodeReview/plugins_checks

Limit relevant checkers with an optional change query
Change-Id: Ieff1fa72b0a2c6399f3d66ecf5d5cb575436fdd2

javatests/com/google/gerrit/plugins/checks/acceptance/testsuite/CheckerOperationsImplTest.java
@@ -286,6 +286,26 @@ public class CheckerOperationsImplTest extends AbstractCheckersTest {
   }
 
   @Test
+  public void queryCanBeUpdated() throws Exception {
+    String checkerUuid = checkerOperations.newChecker().query("f:foo").create();
+
+    checkerOperations.checker(checkerUuid).forUpdate().query("f:bar").update();
+
+    Optional<String> currentQuery = checkerOperations.checker(checkerUuid).get().query();
+    assertThat(currentQuery).hasValue("f:bar");
+  }
+
+  @Test
+  public void queryCanBeCleared() throws Exception {
+    String checkerUuid = checkerOperations.newChecker().query("f:foo").create();
+
+    checkerOperations.checker(checkerUuid).forUpdate().clearQuery().update();
+
+    Optional<String> currentQuery = checkerOperations.checker(checkerUuid).get().query();
+    assertThat(currentQuery).isEmpty();
+  }
+
+  @Test
   public void getCommit() throws Exception {
     CheckerInfo checker = checkersApi.create(createArbitraryCheckerInput()).get();
 

uddr/gerrit-checks

javatests/com/google/gerrit/plugins/checks/acceptance/testsuite/CheckerOperationsImplTest.java
@@ -287,6 +287,26 @@ public class CheckerOperationsImplTest extends AbstractCheckersTest {
   }
 
   @Test
+  public void queryCanBeUpdated() throws Exception {
+    String checkerUuid = checkerOperations.newChecker().query("f:foo").create();
+
+    checkerOperations.checker(checkerUuid).forUpdate().query("f:bar").update();
+
+    Optional<String> currentQuery = checkerOperations.checker(checkerUuid).get().getQuery();
+    assertThat(currentQuery).hasValue("f:bar");
+  }
+
+  @Test
+  public void queryCanBeCleared() throws Exception {
+    String checkerUuid = checkerOperations.newChecker().query("f:foo").create();
+
+    checkerOperations.checker(checkerUuid).forUpdate().clearQuery().update();
+
+    Optional<String> currentQuery = checkerOperations.checker(checkerUuid).get().getQuery();
+    assertThat(currentQuery).isEmpty();
+  }
+
+  @Test
   public void getCommit() throws Exception {
     CheckerInfo checker = checkersApi.create(createArbitraryCheckerInput()).get();
 