GerritCodeReview/plugins_checks

Add tests with invalid checker queries
Checker queries can be invalid in 2 ways:
1. the query is not parseable
2. the query contains non-allowed predicates

Creating checkers with invalid queries is not allowed, however if it
happens the system should still behave properly, hence this should be
tested.

Checkers with invalid queries should be treated the same way as checkers
with valid, non-matching queries. That's already the case but this
change adds tests for this.

Signed-off-by: Edwin Kempin <ekempin@google.com>
Change-Id: I8baca2b117c3196272a0c592ce71cd9a4b733754

javatests/com/google/gerrit/plugins/checks/acceptance/api/CreateCheckIT.java
@@ -172,6 +172,36 @@ public class CreateCheckIT extends AbstractCheckersTest {
   }
 
   @Test
+  public void canCreateCheckForCheckerWithNonAllowedPredicateInQuery() throws Exception {
+    CheckerUuid checkerUuid =
+        checkerOperations.newChecker().repository(project).query("project:foo").create();
+
+    CheckInput input = new CheckInput();
+    input.checkerUuid = checkerUuid.toString();
+    input.state = CheckState.RUNNING;
+
+    checksApiFactory.revision(patchSetId).create(input);
+
+    CheckKey checkKey = CheckKey.create(project, patchSetId, checkerUuid);
+    assertThat(checkOperations.check(checkKey).exists()).isTrue();
+  }
+
+  @Test
+  public void canCreateCheckForCheckerWithInvalidQuery() throws Exception {
+    CheckerUuid checkerUuid =
+        checkerOperations.newChecker().repository(project).query(":foo :bar").create();
+
+    CheckInput input = new CheckInput();
+    input.checkerUuid = checkerUuid.toString();
+    input.state = CheckState.RUNNING;
+
+    checksApiFactory.revision(patchSetId).create(input);
+
+    CheckKey checkKey = CheckKey.create(project, patchSetId, checkerUuid);
+    assertThat(checkOperations.check(checkKey).exists()).isTrue();
+  }
+
+  @Test
   public void cannotCreateCheckWithoutAdministrateCheckers() throws Exception {
     requestScopeOperations.setApiUser(user.getId());
 

javatests/com/google/gerrit/plugins/checks/acceptance/api/GetCheckIT.java
@@ -123,6 +123,30 @@ public class GetCheckIT extends AbstractCheckersTest {
   }
 
   @Test
+  public void getCheckForCheckerWithNonAllowedPredicateInQuery() throws Exception {
+    CheckerUuid checkerUuid =
+        checkerOperations.newChecker().repository(project).query("project:foo").create();
+
+    CheckKey checkKey = CheckKey.create(project, patchSetId, checkerUuid);
+    checkOperations.newCheck(checkKey).setState(CheckState.RUNNING).upsert();
+
+    CheckInfo checkInfo = checksApiFactory.revision(patchSetId).id(checkerUuid).get();
+    assertThat(checkInfo).isEqualTo(checkOperations.check(checkKey).asInfo());
+  }
+
+  @Test
+  public void getCheckForCheckerWithInvalidQuery() throws Exception {
+    CheckerUuid checkerUuid =
+        checkerOperations.newChecker().repository(project).query(":foo :bar").create();
+
+    CheckKey checkKey = CheckKey.create(project, patchSetId, checkerUuid);
+    checkOperations.newCheck(checkKey).setState(CheckState.RUNNING).upsert();
+
+    CheckInfo checkInfo = checksApiFactory.revision(patchSetId).id(checkerUuid).get();
+    assertThat(checkInfo).isEqualTo(checkOperations.check(checkKey).asInfo());
+  }
+
+  @Test
   public void getCheckForDisabledChecker() throws Exception {
     CheckerUuid checkerUuid = checkerOperations.newChecker().repository(project).create();
 

javatests/com/google/gerrit/plugins/checks/acceptance/api/UpdateCheckIT.java
@@ -78,6 +78,28 @@ public class UpdateCheckIT extends AbstractCheckersTest {
   }
 
   @Test
+  public void canUpdateCheckForCheckerWithNonAllowedPredicateInQuery() throws Exception {
+    checkerOperations.checker(checkKey.checkerUuid()).forUpdate().query("project:foo").update();
+
+    CheckInput input = new CheckInput();
+    input.state = CheckState.SUCCESSFUL;
+
+    CheckInfo info = checksApiFactory.revision(patchSetId).id(checkKey.checkerUuid()).update(input);
+    assertThat(info.state).isEqualTo(CheckState.SUCCESSFUL);
+  }
+
+  @Test
+  public void canUpdateCheckForCheckerWithInvalidQuery() throws Exception {
+    checkerOperations.checker(checkKey.checkerUuid()).forUpdate().query(":foo :bar").update();
+
+    CheckInput input = new CheckInput();
+    input.state = CheckState.SUCCESSFUL;
+
+    CheckInfo info = checksApiFactory.revision(patchSetId).id(checkKey.checkerUuid()).update(input);
+    assertThat(info.state).isEqualTo(CheckState.SUCCESSFUL);
+  }
+
+  @Test
   public void canUpdateCheckForCheckerThatDoesNotApplyToTheChange() throws Exception {
     checkerOperations
         .checker(checkKey.checkerUuid())

uddr/gerrit-checks

javatests/com/google/gerrit/plugins/checks/acceptance/api/CreateCheckIT.java
@@ -173,6 +173,36 @@ public class CreateCheckIT extends AbstractCheckersTest {
   }
 
   @Test
+  public void canCreateCheckForCheckerWithUnsupportedOperatorInQuery() throws Exception {
+    CheckerUuid checkerUuid =
+        checkerOperations.newChecker().repository(project).query("project:foo").create();
+
+    CheckInput input = new CheckInput();
+    input.checkerUuid = checkerUuid.toString();
+    input.state = CheckState.RUNNING;
+
+    checksApiFactory.revision(patchSetId).create(input);
+
+    CheckKey checkKey = CheckKey.create(project, patchSetId, checkerUuid);
+    assertThat(checkOperations.check(checkKey).exists()).isTrue();
+  }
+
+  @Test
+  public void canCreateCheckForCheckerWithInvalidQuery() throws Exception {
+    CheckerUuid checkerUuid =
+        checkerOperations.newChecker().repository(project).query(":foo :bar").create();
+
+    CheckInput input = new CheckInput();
+    input.checkerUuid = checkerUuid.toString();
+    input.state = CheckState.RUNNING;
+
+    checksApiFactory.revision(patchSetId).create(input);
+
+    CheckKey checkKey = CheckKey.create(project, patchSetId, checkerUuid);
+    assertThat(checkOperations.check(checkKey).exists()).isTrue();
+  }
+
+  @Test
   public void cannotCreateCheckWithoutAdministrateCheckers() throws Exception {
     requestScopeOperations.setApiUser(user.getId());
 

javatests/com/google/gerrit/plugins/checks/acceptance/api/GetCheckIT.java
@@ -123,6 +123,30 @@ public class GetCheckIT extends AbstractCheckersTest {
   }
 
   @Test
+  public void getCheckForCheckerWithUnsupportedOperatorInQuery() throws Exception {
+    CheckerUuid checkerUuid =
+        checkerOperations.newChecker().repository(project).query("project:foo").create();
+
+    CheckKey checkKey = CheckKey.create(project, patchSetId, checkerUuid);
+    checkOperations.newCheck(checkKey).setState(CheckState.RUNNING).upsert();
+
+    CheckInfo checkInfo = checksApiFactory.revision(patchSetId).id(checkerUuid).get();
+    assertThat(checkInfo).isEqualTo(checkOperations.check(checkKey).asInfo());
+  }
+
+  @Test
+  public void getCheckForCheckerWithInvalidQuery() throws Exception {
+    CheckerUuid checkerUuid =
+        checkerOperations.newChecker().repository(project).query(":foo :bar").create();
+
+    CheckKey checkKey = CheckKey.create(project, patchSetId, checkerUuid);
+    checkOperations.newCheck(checkKey).setState(CheckState.RUNNING).upsert();
+
+    CheckInfo checkInfo = checksApiFactory.revision(patchSetId).id(checkerUuid).get();
+    assertThat(checkInfo).isEqualTo(checkOperations.check(checkKey).asInfo());
+  }
+
+  @Test
   public void getCheckForDisabledChecker() throws Exception {
     CheckerUuid checkerUuid = checkerOperations.newChecker().repository(project).create();
 

javatests/com/google/gerrit/plugins/checks/acceptance/api/UpdateCheckIT.java
@@ -78,6 +78,28 @@ public class UpdateCheckIT extends AbstractCheckersTest {
   }
 
   @Test
+  public void canUpdateCheckForCheckerWithUnsupportedOperatorInQuery() throws Exception {
+    checkerOperations.checker(checkKey.checkerUuid()).forUpdate().query("project:foo").update();
+
+    CheckInput input = new CheckInput();
+    input.state = CheckState.SUCCESSFUL;
+
+    CheckInfo info = checksApiFactory.revision(patchSetId).id(checkKey.checkerUuid()).update(input);
+    assertThat(info.state).isEqualTo(CheckState.SUCCESSFUL);
+  }
+
+  @Test
+  public void canUpdateCheckForCheckerWithInvalidQuery() throws Exception {
+    checkerOperations.checker(checkKey.checkerUuid()).forUpdate().query(":foo :bar").update();
+
+    CheckInput input = new CheckInput();
+    input.state = CheckState.SUCCESSFUL;
+
+    CheckInfo info = checksApiFactory.revision(patchSetId).id(checkKey.checkerUuid()).update(input);
+    assertThat(info.state).isEqualTo(CheckState.SUCCESSFUL);
+  }
+
+  @Test
   public void canUpdateCheckForCheckerThatDoesNotApplyToTheChange() throws Exception {
     checkerOperations
         .checker(checkKey.checkerUuid())