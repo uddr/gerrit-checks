4e91397b14f786ba30f7de149b14796cb855ac2c
Edwin Kempin <ekempin@google.com>
2019-03-27 14:19:44 +0100

    ListChecksIT: Let each test care about the test setup itself
    At the moment ListChecksIT creates some checkers in its setUp() method.
    This makes the tests less readable since for understanding the
    assertions in one test one always has to take the global test setup into
    account (e.g. one needs to scroll to the beginning of the class to see
    the global setup).
    
    It also makes the test assertions more complicated since this way many
    tests do also check conditions that are not relevant for them. Not
    needing to do this simplifies the tests.
    
    Also some pieces of the global test setup were only relevant for some of
    the tests (e.g. setting a checker name, setting check states). So it was
    getting more and more complicated to have a common global test setup
    that suited all tests.
    
    Signed-off-by: Edwin Kempin <ekempin@google.com>
    Change-Id: I6615ecf83f3671db5e90a45cfd3d46c6e978e43a

--- javatests/com/google/gerrit/plugins/checks/acceptance/api/ListChecksIT.java
+++ javatests/com/google/gerrit/plugins/checks/acceptance/api/ListChecksIT.java
@@ -40,43 +40,40 @@ import org.junit.Test;
 
 public class ListChecksIT extends AbstractCheckersTest {
   private PatchSet.Id patchSetId;
-  private CheckKey checkKey1;
-  private CheckKey checkKey2;
 
   @Before
   public void setUp() throws Exception {
     patchSetId = createChange().getPatchSetId();
-
-    CheckerUuid checker1Uuid = checkerOperations.newChecker().repository(project).create();
-    CheckerUuid checker2Uuid =
-        checkerOperations.newChecker().name("Checker Two").repository(project).create();
-
-    checkKey1 = CheckKey.create(project, patchSetId, checker1Uuid);
-    checkOperations.newCheck(checkKey1).setState(CheckState.RUNNING).upsert();
-
-    checkKey2 = CheckKey.create(project, patchSetId, checker2Uuid);
-    checkOperations.newCheck(checkKey2).setState(CheckState.RUNNING).upsert();
   }
 
   @Test
   public void listAll() throws Exception {
+    CheckerUuid checkerUuid1 = checkerOperations.newChecker().repository(project).create();
+    CheckerUuid checkerUuid2 = checkerOperations.newChecker().repository(project).create();
+
+    CheckKey checkKey1 = CheckKey.create(project, patchSetId, checkerUuid1);
+    checkOperations.newCheck(checkKey1).setState(CheckState.RUNNING).upsert();
+
+    CheckKey checkKey2 = CheckKey.create(project, patchSetId, checkerUuid2);
+    checkOperations.newCheck(checkKey2).setState(CheckState.FAILED).upsert();
+
     Collection<CheckInfo> checks = checksApiFactory.revision(patchSetId).list();
 
     CheckInfo expected1 = new CheckInfo();
-    expected1.repository = checkKey1.repository().get();
-    expected1.changeNumber = checkKey1.patchSet().getParentKey().get();
-    expected1.patchSetId = checkKey1.patchSet().get();
-    expected1.checkerUuid = checkKey1.checkerUuid().get();
+    expected1.repository = project.get();
+    expected1.changeNumber = patchSetId.getParentKey().get();
+    expected1.patchSetId = patchSetId.get();
+    expected1.checkerUuid = checkerUuid1.get();
     expected1.state = CheckState.RUNNING;
     expected1.created = checkOperations.check(checkKey1).get().created();
     expected1.updated = expected1.created;
 
     CheckInfo expected2 = new CheckInfo();
-    expected2.repository = checkKey2.repository().get();
-    expected2.changeNumber = checkKey2.patchSet().getParentKey().get();
-    expected2.patchSetId = checkKey2.patchSet().get();
-    expected2.checkerUuid = checkKey2.checkerUuid().get();
-    expected2.state = CheckState.RUNNING;
+    expected2.repository = project.get();
+    expected2.changeNumber = patchSetId.getParentKey().get();
+    expected2.patchSetId = patchSetId.get();
+    expected2.checkerUuid = checkerUuid2.get();
+    expected2.state = CheckState.FAILED;
     expected2.created = checkOperations.check(checkKey2).get().created();
     expected2.updated = expected2.created;
 
@@ -85,29 +82,40 @@ public class ListChecksIT extends AbstractCheckersTest {
 
   @Test
   public void listAllWithOptions() throws Exception {
+    String checkerName1 = "Checker One";
+    CheckerUuid checkerUuid1 =
+        checkerOperations.newChecker().name(checkerName1).repository(project).create();
+    CheckerUuid checkerUuid2 = checkerOperations.newChecker().repository(project).create();
+
+    CheckKey checkKey1 = CheckKey.create(project, patchSetId, checkerUuid1);
+    checkOperations.newCheck(checkKey1).setState(CheckState.RUNNING).upsert();
+
+    CheckKey checkKey2 = CheckKey.create(project, patchSetId, checkerUuid2);
+    checkOperations.newCheck(checkKey2).setState(CheckState.FAILED).upsert();
+
     Collection<CheckInfo> checks =
         checksApiFactory.revision(patchSetId).list(ListChecksOption.CHECKER);
 
     CheckInfo expected1 = new CheckInfo();
-    expected1.repository = checkKey1.repository().get();
-    expected1.changeNumber = checkKey1.patchSet().getParentKey().get();
-    expected1.patchSetId = checkKey1.patchSet().get();
-    expected1.checkerUuid = checkKey1.checkerUuid().get();
+    expected1.repository = project.get();
+    expected1.changeNumber = patchSetId.getParentKey().get();
+    expected1.patchSetId = patchSetId.get();
+    expected1.checkerUuid = checkerUuid1.get();
     expected1.state = CheckState.RUNNING;
     expected1.created = checkOperations.check(checkKey1).get().created();
     expected1.updated = expected1.created;
+    expected1.checkerName = checkerName1;
     expected1.blocking = ImmutableSet.of();
     expected1.checkerStatus = CheckerStatus.ENABLED;
 
     CheckInfo expected2 = new CheckInfo();
-    expected2.repository = checkKey2.repository().get();
-    expected2.changeNumber = checkKey2.patchSet().getParentKey().get();
-    expected2.patchSetId = checkKey2.patchSet().get();
-    expected2.checkerUuid = checkKey2.checkerUuid().get();
-    expected2.state = CheckState.RUNNING;
+    expected2.repository = project.get();
+    expected2.changeNumber = patchSetId.getParentKey().get();
+    expected2.patchSetId = patchSetId.get();
+    expected2.checkerUuid = checkerUuid2.get();
+    expected2.state = CheckState.FAILED;
     expected2.created = checkOperations.check(checkKey2).get().created();
     expected2.updated = expected2.created;
-    expected2.checkerName = "Checker Two";
     expected2.blocking = ImmutableSet.of();
     expected2.checkerStatus = CheckerStatus.ENABLED;
 
@@ -116,141 +124,127 @@ public class ListChecksIT extends AbstractCheckersTest {
 
   @Test
   public void listAllWithOptionsSkipsPopulatingCheckerFieldsForInvalidCheckers() throws Exception {
-    checkerOperations.checker(checkKey1.checkerUuid()).forUpdate().forceInvalidConfig().update();
+    String checkerName1 = "Checker One";
+    CheckerUuid checkerUuid1 =
+        checkerOperations.newChecker().name(checkerName1).repository(project).create();
+    CheckerUuid checkerUuid2 = checkerOperations.newChecker().repository(project).create();
+    checkOperations.newCheck(CheckKey.create(project, patchSetId, checkerUuid1)).upsert();
+    checkOperations.newCheck(CheckKey.create(project, patchSetId, checkerUuid2)).upsert();
+    checkerOperations.checker(checkerUuid2).forUpdate().forceInvalidConfig().update();
 
     List<CheckInfo> checks = checksApiFactory.revision(patchSetId).list(ListChecksOption.CHECKER);
 
     assertThat(checks).hasSize(2);
 
     Optional<CheckInfo> maybeCheck1 =
-        checks.stream().filter(c -> c.checkerUuid.equals(checkKey1.checkerUuid().get())).findAny();
+        checks.stream().filter(c -> c.checkerUuid.equals(checkerUuid1.get())).findAny();
     assertThat(maybeCheck1).isPresent();
     CheckInfo check1 = maybeCheck1.get();
-    assertThat(check1.checkerName).isNull();
-    assertThat(check1.blocking).isNull();
-    assertThat(check1.checkerStatus).isNull();
+    assertThat(check1.checkerName).isEqualTo(checkerName1);
+    assertThat(check1.blocking).isEmpty();
+    assertThat(check1.checkerStatus).isEqualTo(CheckerStatus.ENABLED);
 
     Optional<CheckInfo> maybeCheck2 =
-        checks.stream().filter(c -> c.checkerUuid.equals(checkKey2.checkerUuid().get())).findAny();
+        checks.stream().filter(c -> c.checkerUuid.equals(checkerUuid2.get())).findAny();
     assertThat(maybeCheck2).isPresent();
     CheckInfo check2 = maybeCheck2.get();
-    assertThat(check2.checkerName).isEqualTo("Checker Two");
-    assertThat(check2.blocking).isEmpty();
-    assertThat(check2.checkerStatus).isEqualTo(CheckerStatus.ENABLED);
+    assertThat(check2.checkerName).isNull();
+    assertThat(check2.blocking).isNull();
+    assertThat(check2.checkerStatus).isNull();
   }
 
   @Test
   public void listIncludesCheckFromCheckerThatDoesNotApplyToTheProject() throws Exception {
+    CheckerUuid checkerUuid = checkerOperations.newChecker().repository(project).create();
+    checkOperations.newCheck(CheckKey.create(project, patchSetId, checkerUuid)).upsert();
     Project.NameKey otherProject = createProjectOverAPI("other", null, true, null);
-    checkerOperations
-        .checker(checkKey2.checkerUuid())
-        .forUpdate()
-        .repository(otherProject)
-        .update();
+    checkerOperations.checker(checkerUuid).forUpdate().repository(otherProject).update();
 
-    Collection<CheckInfo> checks = checksApiFactory.revision(patchSetId).list();
-    assertThat(checks)
-        .containsExactly(
-            checkOperations.check(checkKey1).asInfo(), checkOperations.check(checkKey2).asInfo());
+    assertThat(checksApiFactory.revision(patchSetId).list()).hasSize(1);
   }
 
   @Test
   public void listIncludesCheckFromCheckerThatDoesNotApplyToTheChange() throws Exception {
-    checkerOperations
-        .checker(checkKey2.checkerUuid())
-        .forUpdate()
-        .query("message:not-matching")
-        .update();
+    CheckerUuid checkerUuid = checkerOperations.newChecker().repository(project).create();
 
-    Collection<CheckInfo> checks = checksApiFactory.revision(patchSetId).list();
-    assertThat(checks)
-        .containsExactly(
-            checkOperations.check(checkKey1).asInfo(), checkOperations.check(checkKey2).asInfo());
+    CheckKey checkKey = CheckKey.create(project, patchSetId, checkerUuid);
+    checkOperations.newCheck(checkKey).upsert();
+
+    checkerOperations.checker(checkerUuid).forUpdate().query("message:not-matching").update();
+
+    assertThat(checksApiFactory.revision(patchSetId).list()).hasSize(1);
   }
 
   @Test
   public void listIncludesCheckFromDisabledChecker() throws Exception {
-    checkerOperations.checker(checkKey2.checkerUuid()).forUpdate().disable().update();
+    CheckerUuid checkerUuid = checkerOperations.newChecker().repository(project).create();
+    checkOperations.newCheck(CheckKey.create(project, patchSetId, checkerUuid)).upsert();
+    checkerOperations.checker(checkerUuid).forUpdate().disable().update();
 
-    Collection<CheckInfo> checks = checksApiFactory.revision(patchSetId).list();
-    assertThat(checks)
-        .containsExactly(
-            checkOperations.check(checkKey1).asInfo(), checkOperations.check(checkKey2).asInfo());
+    assertThat(checksApiFactory.revision(patchSetId).list()).hasSize(1);
   }
 
   @Test
   public void listIncludesCheckFromInvalidChecker() throws Exception {
-    checkerOperations.checker(checkKey2.checkerUuid()).forUpdate().forceInvalidConfig().update();
+    CheckerUuid checkerUuid = checkerOperations.newChecker().repository(project).create();
+    checkOperations.newCheck(CheckKey.create(project, patchSetId, checkerUuid)).upsert();
+    checkerOperations.checker(checkerUuid).forUpdate().forceInvalidConfig().update();
 
-    Collection<CheckInfo> checks = checksApiFactory.revision(patchSetId).list();
-    assertThat(checks)
-        .containsExactly(
-            checkOperations.check(checkKey1).asInfo(), checkOperations.check(checkKey2).asInfo());
+    assertThat(checksApiFactory.revision(patchSetId).list()).hasSize(1);
   }
 
   @Test
   public void listIncludesCheckFromNonExistingChecker() throws Exception {
-    checkerOperations.checker(checkKey2.checkerUuid()).forUpdate().deleteRef().update();
+    CheckerUuid checkerUuid = checkerOperations.newChecker().repository(project).create();
+    checkOperations.newCheck(CheckKey.create(project, patchSetId, checkerUuid)).upsert();
+    checkerOperations.checker(checkerUuid).forUpdate().deleteRef().update();
 
-    Collection<CheckInfo> checks = checksApiFactory.revision(patchSetId).list();
-    assertThat(checks)
-        .containsExactly(
-            checkOperations.check(checkKey1).asInfo(), checkOperations.check(checkKey2).asInfo());
+    assertThat(checksApiFactory.revision(patchSetId).list()).hasSize(1);
   }
 
   @Test
   public void listBackfillsForRelevantChecker() throws Exception {
     String topic = name("topic");
-    Change.Id changeId = patchSetId.getParentKey();
-    CheckerUuid checker3Uuid =
+    CheckerUuid checkerUuid =
         checkerOperations.newChecker().repository(project).query("topic:" + topic).create();
-    CheckKey checkKey3 = CheckKey.create(project, patchSetId, checker3Uuid);
 
-    CheckInfo checkInfo1 = checkOperations.check(checkKey1).asInfo();
-    CheckInfo checkInfo2 = checkOperations.check(checkKey2).asInfo();
-    assertThat(checksApiFactory.revision(patchSetId).list())
-        .containsExactly(checkInfo1, checkInfo2);
+    assertThat(checksApiFactory.revision(patchSetId).list()).isEmpty();
 
-    // Update change to match checker3's query.
-    gApi.changes().id(changeId.get()).topic(topic);
+    // Update change to match checker's query.
+    gApi.changes().id(patchSetId.getParentKey().get()).topic(topic);
 
     Timestamp psCreated = getPatchSetCreated(patchSetId.getParentKey());
-    CheckInfo checkInfo3 = new CheckInfo();
-    checkInfo3.repository = checkKey3.repository().get();
-    checkInfo3.changeNumber = checkKey3.patchSet().getParentKey().get();
-    checkInfo3.patchSetId = checkKey3.patchSet().get();
-    checkInfo3.checkerUuid = checkKey3.checkerUuid().get();
-    checkInfo3.state = CheckState.NOT_STARTED;
-    checkInfo3.created = psCreated;
-    checkInfo3.updated = psCreated;
-    assertThat(checksApiFactory.revision(patchSetId).list())
-        .containsExactly(checkInfo1, checkInfo2, checkInfo3);
+    CheckInfo checkInfo = new CheckInfo();
+    checkInfo.repository = project.get();
+    checkInfo.changeNumber = patchSetId.getParentKey().get();
+    checkInfo.patchSetId = patchSetId.get();
+    checkInfo.checkerUuid = checkerUuid.get();
+    checkInfo.state = CheckState.NOT_STARTED;
+    checkInfo.created = psCreated;
+    checkInfo.updated = psCreated;
+    assertThat(checksApiFactory.revision(patchSetId).list()).containsExactly(checkInfo);
   }
 
   @Test
   public void listDoesntBackfillForDisabledChecker() throws Exception {
-    CheckerUuid checker3Uuid = checkerOperations.newChecker().repository(project).create();
-    CheckKey checkKey3 = CheckKey.create(project, patchSetId, checker3Uuid);
+    CheckerUuid checkerUuid = checkerOperations.newChecker().repository(project).create();
+    CheckKey checkKey = CheckKey.create(project, patchSetId, checkerUuid);
 
-    CheckInfo checkInfo1 = checkOperations.check(checkKey1).asInfo();
-    CheckInfo checkInfo2 = checkOperations.check(checkKey2).asInfo();
     Timestamp psCreated = getPatchSetCreated(patchSetId.getParentKey());
-    CheckInfo checkInfo3 = new CheckInfo();
-    checkInfo3.repository = checkKey3.repository().get();
-    checkInfo3.changeNumber = checkKey3.patchSet().getParentKey().get();
-    checkInfo3.patchSetId = checkKey3.patchSet().get();
-    checkInfo3.checkerUuid = checkKey3.checkerUuid().get();
-    checkInfo3.state = CheckState.NOT_STARTED;
-    checkInfo3.created = psCreated;
-    checkInfo3.updated = psCreated;
-    assertThat(checksApiFactory.revision(patchSetId).list())
-        .containsExactly(checkInfo1, checkInfo2, checkInfo3);
+    CheckInfo checkInfo = new CheckInfo();
+    checkInfo.repository = checkKey.repository().get();
+    checkInfo.changeNumber = checkKey.patchSet().getParentKey().get();
+    checkInfo.patchSetId = checkKey.patchSet().get();
+    checkInfo.checkerUuid = checkKey.checkerUuid().get();
+    checkInfo.state = CheckState.NOT_STARTED;
+    checkInfo.created = psCreated;
+    checkInfo.updated = psCreated;
+    assertThat(checksApiFactory.revision(patchSetId).list()).containsExactly(checkInfo);
 
-    // Disable checker3.
-    checkerOperations.checker(checker3Uuid).forUpdate().disable().update();
+    // Disable checker.
+    checkerOperations.checker(checkerUuid).forUpdate().disable().update();
 
-    assertThat(checksApiFactory.revision(patchSetId).list())
-        .containsExactly(checkInfo1, checkInfo2);
+    assertThat(checksApiFactory.revision(patchSetId).list()).isEmpty();
   }
 
   private Timestamp getPatchSetCreated(Change.Id changeId) throws RestApiException {
