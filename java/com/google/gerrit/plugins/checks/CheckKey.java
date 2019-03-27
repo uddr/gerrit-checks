GerritCodeReview/plugins_checks

Consistently use the name 'repository' in the checks API
Some parts of the checks API used the term 'repository' (CheckerInfo)
while other parts of the checks API used the term 'project' (CheckInfo,
CheckablePatchSetInfo). The checks API should be consistent with the
naming.

Reasons for using the term 'repository':
- this is the term that is used in the UI
- the checks API will likely be used by people that are not familiar
  with Gerrit yet (and hence are not familiar the 'project' term), e.g.
  owners of analyzers that want to integrate with Gerrit
- it's the same name that is used by other git services

Reasons for using the term 'project':
- it's consistent with the Gerrit core REST API
- it's the name that is familiar for existing Gerrit users

I discussed this with aliceks@ and we think that 'repository' is the
better choice. Consistency with the Gerrit core REST API is not that
important since the checks API is rather independent from it.

Signed-off-by: Edwin Kempin <ekempin@google.com>
Change-Id: I0e1d68277e4829f270e5f247673757eff9990fea

java/com/google/gerrit/plugins/checks/CheckKey.java
@@ -21,14 +21,14 @@ import com.google.gerrit.reviewdb.client.Project;
 /** Fields to identify a check. */
 @AutoValue
 public abstract class CheckKey {
-  public abstract Project.NameKey project();
+  public abstract Project.NameKey repository();
 
   public abstract PatchSet.Id patchSet();
 
   public abstract CheckerUuid checkerUuid();
 
   public static CheckKey create(
-      Project.NameKey project, PatchSet.Id patchSet, CheckerUuid checkerUuid) {
-    return new AutoValue_CheckKey(project, patchSet, checkerUuid);
+      Project.NameKey repository, PatchSet.Id patchSet, CheckerUuid checkerUuid) {
+    return new AutoValue_CheckKey(repository, patchSet, checkerUuid);
   }
 }

java/com/google/gerrit/plugins/checks/testing/PendingChecksInfoSubject.java
@@ -35,8 +35,10 @@ public class PendingChecksInfoSubject extends Subject<PendingChecksInfoSubject, 
     super(metadata, actual);
   }
 
-  public void hasProject(Project.NameKey expectedProject) {
-    check("patchSet().project()").that(patchSet().project).isEqualTo(expectedProject.get());
+  public void hasRepository(Project.NameKey expectedRepository) {
+    check("patchSet().repository()")
+        .that(patchSet().repository)
+        .isEqualTo(expectedRepository.get());
   }
 
   public void hasPatchSet(PatchSet.Id expectedPatchSetId) {

uddr/gerrit-checks

java/com/google/gerrit/plugins/checks/CheckKey.java
@@ -21,14 +21,14 @@ import com.google.gerrit.reviewdb.client.Project;
 /** Fields to identify a check. */
 @AutoValue
 public abstract class CheckKey {
-  public abstract Project.NameKey project();
+  public abstract Project.NameKey repository();
 
   public abstract PatchSet.Id patchSet();
 
   public abstract CheckerUuid checkerUuid();
 
   public static CheckKey create(
-      Project.NameKey project, PatchSet.Id patchSet, CheckerUuid checkerUuid) {
-    return new AutoValue_CheckKey(project, patchSet, checkerUuid);
+      Project.NameKey repositoryName, PatchSet.Id patchSetId, CheckerUuid checkerUuid) {
+    return new AutoValue_CheckKey(repositoryName, patchSetId, checkerUuid);
   }
 }

java/com/google/gerrit/plugins/checks/testing/PendingChecksInfoSubject.java
@@ -35,8 +35,10 @@ public class PendingChecksInfoSubject extends Subject<PendingChecksInfoSubject, 
     super(metadata, actual);
   }
 
-  public void hasProject(Project.NameKey expectedProject) {
-    check("patchSet().project()").that(patchSet().project).isEqualTo(expectedProject.get());
+  public void hasRepository(Project.NameKey expectedRepositoryName) {
+    check("patchSet().repository()")
+        .that(patchSet().repository)
+        .isEqualTo(expectedRepositoryName.get());
   }
 
   public void hasPatchSet(PatchSet.Id expectedPatchSetId) {