GerritCodeReview/plugins_checks

Change List Pending Checks REST endpoint to accept a query as input
At the moment the List Pending Checks REST endpoint supports 2 URL
parameters:

1. checker: checker UUID (requried)
2. state: check state (optional, can be specified multiple times)

Later we would need another URL parameter for the checker scheme.

In the future the List Pending Checks REST endpoint should be
implemented on top of a new check index. This means the List Pending
Checks REST endpoint will execute a query on the check index.

Keeping this in mind we want to make the List Pending Checks REST
endpoint already now consistent with other Query REST endpoints (e.g.
QueryAccounts, QueryChanges) so that we don't break callers when we
migrate this REST endpoint on top of a check index.

All existing query REST endpoints take a query string as input (via a
'query' URL parameter). To be consistent the List Pending REST endpoint
should also expect a query string as input.

The current URL parameters are transitioned to operators in the input
query. This means instead of

  GET /plugins/checks~checks.pending/?checker=<checker-uuid>&state=<state>

one would now call

  GET /plugins/checks~checks.pending/?query=checker:<checker-uuid>+state:<state>

With this syntax we allow users to make full use of the flexibility that
an index provides, once we have a check index.

While there is no check index yet there are some limitations for the
query:
1. exactly 1 checker predicate must be used
2. the checker predicate must either be the root predicate
   (query = "checker:<checker-uuid>") or the root predicate must be an
   'and' predicate that has a checker predicate as immediate child
   (query = "checker:<checker-uuid> AND ...")

These limitations will be removed when we implement a check index.

For now we implement the List Pending Checks REST endpoint like this:
1. implement a CheckQueryBuilder which supports a 'checker' and a
   'state' operator, use the CheckQueryBuilder to parse the query
2. validate

java/com/google/gerrit/plugins/checks/api/ListPendingChecks.java
@@ -14,13 +14,17 @@
 
 package com.google.gerrit.plugins.checks.api;
 
+import static com.google.common.base.Preconditions.checkState;
+
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.google.gerrit.extensions.restapi.BadRequestException;
 import com.google.gerrit.extensions.restapi.RestApiException;
 import com.google.gerrit.extensions.restapi.RestReadView;
 import com.google.gerrit.extensions.restapi.TopLevelResource;
-import com.google.gerrit.extensions.restapi.UnprocessableEntityException;
+import com.google.gerrit.index.query.AndPredicate;
+import com.google.gerrit.index.query.Predicate;
+import com.google.gerrit.index.query.QueryParseException;
 import com.google.gerrit.plugins.checks.Check;
 import com.google.gerrit.plugins.checks.CheckKey;
 import com.google.gerrit.plugins.checks.Checker;
@@ -28,6 +32,9 @@ import com.google.gerrit.plugins.checks.CheckerUuid;
 import com.google.gerrit.plugins.checks.Checkers;
 import com.google.gerrit.plugins.checks.Checks;
 import com.google.gerrit.plugins.checks.Checks.GetCheckOptions;
+import com.google.gerrit.plugins.checks.index.CheckQueryBuilder;
+import com.google.gerrit.plugins.checks.index.CheckStatePredicate;
+import com.google.gerrit.plugins.checks.index.CheckerPredicate;
 import com.google.gerrit.reviewdb.client.PatchSet;
 import com.google.gerrit.reviewdb.client.Project;
 import com.google.gerrit.server.query.change.ChangeData;
@@ -45,36 +52,33 @@ import org.eclipse.jgit.errors.ConfigInvalidException;
 import org.kohsuke.args4j.Option;
 
 public class ListPendingChecks implements RestReadView<TopLevelResource> {
+  private final CheckQueryBuilder checkQueryBuilder;
   private final Checkers checkers;
   private final Checks checks;
   private final RetryHelper retryHelper;
   private final Provider<ChangeQueryBuilder> queryBuilderProvider;
   private final Provider<ChangeQueryProcessor> changeQueryProcessorProvider;
 
-  private CheckerUuid checkerUuid;
-  private List<CheckState> states = new ArrayList<>(CheckState.values().length);
+  private String queryString;
 
   @Option(
-      name = "--checker",
-      metaVar = "UUID",
-      usage = "checker UUID formatted as '<scheme>:<id>'",
-      handler = CheckerUuidHandler.class)
-  public void setChecker(CheckerUuid checkerUuid) {
-    this.checkerUuid = checkerUuid;
-  }
-
-  @Option(name = "--state", metaVar = "STATE", usage = "check state")
-  public void addState(CheckState state) {
-    this.states.add(state);
+      name = "--query",
+      aliases = {"-q"},
+      metaVar = "QUERY",
+      usage = "check query")
+  public void setQuery(String queryString) {
+    this.queryString = queryString;
   }
 
   @Inject
   public ListPendingChecks(
+      CheckQueryBuilder checkQueryBuilder,
       Checkers checkers,
       Checks checks,
       RetryHelper retryHelper,
       Provider<ChangeQueryBuilder> queryBuilderProvider,
       Provider<ChangeQueryProcessor> changeQueryProcessorProvider) {
+    this.checkQueryBuilder = checkQueryBuilder;
     this.checkers = checkers;
     this.checks = checks;
     this.retryHelper = retryHelper;
@@ -85,76 +89,136 @@ public class ListPendingChecks implements RestReadView<TopLevelResource> {
   @Override
   public List<PendingChecksInfo> apply(TopLevelResource resource)
       throws RestApiException, IOException, ConfigInvalidException, OrmException {
-    if (states.isEmpty()) {
-      // If no state was specified, assume NOT_STARTED by default.
-      states.add(CheckState.NOT_STARTED);
+    if (queryString == null) {
+      throw new BadRequestException("query is required");
     }
 
-    if (checkerUuid == null) {
-      throw new BadRequestException("checker UUID is required");
+    Predicate<Check> query = validateQuery(parseQuery(queryString));
+    if (!hasStatePredicate(query)) {
+      query = Predicate.and(new CheckStatePredicate(CheckState.NOT_STARTED), query);
     }
 
-    Checker checker =
-        checkers
-            .getChecker(checkerUuid)
-            .orElseThrow(
-                () ->
-                    new UnprocessableEntityException(
-                        String.format("checker %s not found", checkerUuid)));
-
-    if (checker.getStatus() == CheckerStatus.DISABLED) {
+    Optional<Checker> checker = checkers.getChecker(getCheckerUuidFromQuery(query));
+    if (!checker.isPresent() || checker.get().isDisabled()) {
       return ImmutableList.of();
     }
 
     // The query system can only match against the current patch set; ignore non-current patch sets
     // for now.
     List<ChangeData> changes =
-        checker.queryMatchingChanges(
-            retryHelper, queryBuilderProvider.get(), changeQueryProcessorProvider);
+        checker
+            .get()
+            .queryMatchingChanges(
+                retryHelper, queryBuilderProvider.get(), changeQueryProcessorProvider);
+    CheckerUuid checkerUuid = checker.get().getUuid();
     List<PendingChecksInfo> pendingChecks = new ArrayList<>(changes.size());
     for (ChangeData cd : changes) {
-      getPostFilteredPendingChecks(cd.project(), cd.currentPatchSet().getId())
-          .ifPresent(pendingChecks::add);
+      PatchSet patchSet = cd.currentPatchSet();
+      CheckKey checkKey = CheckKey.create(cd.project(), patchSet.getId(), checkerUuid);
+
+      // Backfill if check is not present.
+      // Backfilling is only done for relevant checkers (checkers where the repository and the query
+      // matches the change). Since the change was found by executing the query of the checker we
+      // know
+      // that the checker is relevant for this patch set and hence backfilling should be done.
+      Check check =
+          checks
+              .getCheck(checkKey, GetCheckOptions.defaults())
+              .orElseGet(() -> Check.newBackfilledCheck(cd, patchSet, checker.get()));
+
+      if (query.asMatchable().match(check)) {
+        pendingChecks.add(createPendingChecksInfo(cd.project(), patchSet, checkerUuid, check));
+      }
     }
     return pendingChecks;
   }
 
-  private Optional<PendingChecksInfo> getPostFilteredPendingChecks(
-      Project.NameKey project, PatchSet.Id patchSetId) throws OrmException, IOException {
-    CheckState checkState = getCheckState(project, patchSetId);
-    if (!states.contains(checkState)) {
-      return Optional.empty();
+  private Predicate<Check> parseQuery(String query) throws BadRequestException {
+    try {
+      return checkQueryBuilder.parse(query.trim());
+    } catch (QueryParseException e) {
+      throw new BadRequestException(e.getMessage());
     }
-    return Optional.of(createPendingChecksInfo(project, patchSetId, checkerUuid, checkState));
   }
 
-  private CheckState getCheckState(Project.NameKey project, PatchSet.Id patchSetId)
-      throws OrmException, IOException {
-    Optional<Check> check =
-        checks.getCheck(
-            CheckKey.create(project, patchSetId, checkerUuid), GetCheckOptions.defaults());
+  private static Predicate<Check> validateQuery(Predicate<Check> predicate)
+      throws BadRequestException {
+    if (countCheckerPredicates(predicate) != 1)
+      throw new BadRequestException(
+          String.format(
+              "query must contain exactly 1 '%s' operator", CheckQueryBuilder.FIELD_CHECKER));
 
-    // Backfill if check is not present.
-    // Backfilling is only done for relevant checkers (checkers where the repository and the query
-    // matches the change). Since the change was found by executing the query of the checker we know
-    // that the checker is relevant for this patch set and hence backfilling should be done.
-    return check.map(Check::state).orElse(CheckState.NOT_STARTED);
+    if (predicate instanceof AndPredicate) {
+      if (!predicate.getChildren().stream().anyMatch(CheckerPredicate.class::isInstance)) {
+        throw new BadRequestException(
+            String.format(
+                "query must be '%s=<checker-uuid>' or '%s=<checker-uuid> AND <other-operators>'",
+                CheckQueryBuilder.FIELD_CHECKER, CheckQueryBuilder.FIELD_CHECKER));
+      }
+    } else if (!(predicate instanceof CheckerPredicate)) {
+      throw new BadRequestException(
+          String.format(
+              "query must be '%s=<checker-uuid>' or '%s=<checker-uuid> AND <other-operators>'",
+              CheckQueryBuilder.FIELD_CHECKER, CheckQueryBuilder.FIELD_CHECKER));
+    }
+    return predicate;
+  }
+
+  private static boolean hasStatePredicate(Predicate<Check> predicate) {
+    if (predicate instanceof CheckStatePredicate) {
+      return true;
+    }
+    if (predicate.getChildCount() == 0) {
+      return false;
+    }
+    return predicate.getChildren().stream().anyMatch(ListPendingChecks::hasStatePredicate);
+  }
+
+  private static int countCheckerPredicates(Predicate<Check> predicate) {
+    if (predicate instanceof CheckerPredicate) {
+      return 1;
+    }
+    if (predicate.getChildCount() == 0) {
+      return 0;
+    }
+    return predicate.getChildren().stream()
+        .mapToInt(ListPendingChecks::countCheckerPredicates)
+        .sum();
+  }
+
+  private static CheckerUuid getCheckerUuidFromQuery(Predicate<Check> predicate) {
+    // the query validation (see #validateQuery(Predicate<Check>)) ensures that there is exactly 1
+    // CheckerPredicate and that it is on the first or second level of the predicate tree.
+
+    if (predicate instanceof CheckerPredicate) {
+      return ((CheckerPredicate) predicate).getCheckerUuid();
+    }
+
+    checkState(predicate.getChildCount() > 0, "no checker predicate found: %s", predicate);
+    Optional<CheckerPredicate> checkerPredicate =
+        predicate.getChildren().stream()
+            .filter(CheckerPredicate.class::isInstance)
+            .map(p -> (CheckerPredicate) p)
+            .findAny();
+    return checkerPredicate
+        .map(CheckerPredicate::getCheckerUuid)
+        .orElseThrow(
+            () ->
+                new IllegalStateException(
+                    String.format("no checker predicate found: %s", predicate)));
   }
 
   private static PendingChecksInfo createPendingChecksInfo(
-      Project.NameKey project,
-      PatchSet.Id patchSetId,
-      CheckerUuid checkerUuid,
-      CheckState checkState) {
+      Project.NameKey project, PatchSet patchSet, CheckerUuid checkerUuid, Check check) {
     PendingChecksInfo pendingChecksInfo = new PendingChecksInfo();
 
     pendingChecksInfo.patchSet = new CheckablePatchSetInfo();
     pendingChecksInfo.patchSet.project = project.get();
-    pendingChecksInfo.patchSet.changeNumber = patchSetId.getParentKey().get();
-    pendingChecksInfo.patchSet.patchSetId = patchSetId.get();
+    pendingChecksInfo.patchSet.changeNumber = patchSet.getId().getParentKey().get();
+    pendingChecksInfo.patchSet.patchSetId = patchSet.getPatchSetId();
 
     pendingChecksInfo.pendingChecks =
-        ImmutableMap.of(checkerUuid.toString(), new PendingCheckInfo(checkState));
+        ImmutableMap.of(checkerUuid.toString(), new PendingCheckInfo(check.state()));
 
     return pendingChecksInfo;
   }

java/com/google/gerrit/plugins/checks/index/CheckerPredicate.java
@@ -1,0 +1,47 @@
+// Copyright (C) 2019 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.plugins.checks.index;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import com.google.gerrit.index.query.QueryParseException;
+import com.google.gerrit.plugins.checks.Check;
+import com.google.gerrit.plugins.checks.CheckerUuid;
+import com.google.gwtorm.server.OrmException;
+
+public class CheckerPredicate extends CheckPredicate {
+  public static CheckerPredicate parse(String value) throws QueryParseException {
+    return new CheckerPredicate(
+        CheckerUuid.tryParse(value)
+            .orElseThrow(
+                () -> new QueryParseException(String.format("invalid checker UUID: %s", value))));
+  }
+
+  private final CheckerUuid checkerUuid;
+
+  public CheckerPredicate(CheckerUuid checkerUuid) {
+    super(CheckQueryBuilder.FIELD_CHECKER, checkerUuid.toString());
+    this.checkerUuid = checkNotNull(checkerUuid, "checkerUuid");
+  }
+
+  @Override
+  public boolean match(Check check) throws OrmException {
+    return checkerUuid.equals(check.key().checkerUuid());
+  }
+
+  public CheckerUuid getCheckerUuid() {
+    return checkerUuid;
+  }
+}

uddr/gerrit-checks

java/com/google/gerrit/plugins/checks/api/ListPendingChecks.java
@@ -14,13 +14,17 @@
 
 package com.google.gerrit.plugins.checks.api;
 
+import static com.google.common.base.Preconditions.checkState;
+
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.google.gerrit.extensions.restapi.BadRequestException;
 import com.google.gerrit.extensions.restapi.RestApiException;
 import com.google.gerrit.extensions.restapi.RestReadView;
 import com.google.gerrit.extensions.restapi.TopLevelResource;
-import com.google.gerrit.extensions.restapi.UnprocessableEntityException;
+import com.google.gerrit.index.query.AndPredicate;
+import com.google.gerrit.index.query.Predicate;
+import com.google.gerrit.index.query.QueryParseException;
 import com.google.gerrit.plugins.checks.Check;
 import com.google.gerrit.plugins.checks.CheckKey;
 import com.google.gerrit.plugins.checks.Checker;
@@ -28,6 +32,9 @@ import com.google.gerrit.plugins.checks.CheckerUuid;
 import com.google.gerrit.plugins.checks.Checkers;
 import com.google.gerrit.plugins.checks.Checks;
 import com.google.gerrit.plugins.checks.Checks.GetCheckOptions;
+import com.google.gerrit.plugins.checks.index.CheckQueryBuilder;
+import com.google.gerrit.plugins.checks.index.CheckStatePredicate;
+import com.google.gerrit.plugins.checks.index.CheckerPredicate;
 import com.google.gerrit.reviewdb.client.PatchSet;
 import com.google.gerrit.reviewdb.client.Project;
 import com.google.gerrit.server.query.change.ChangeData;
@@ -45,36 +52,33 @@ import org.eclipse.jgit.errors.ConfigInvalidException;
 import org.kohsuke.args4j.Option;
 
 public class ListPendingChecks implements RestReadView<TopLevelResource> {
+  private final CheckQueryBuilder checkQueryBuilder;
   private final Checkers checkers;
   private final Checks checks;
   private final RetryHelper retryHelper;
   private final Provider<ChangeQueryBuilder> queryBuilderProvider;
   private final Provider<ChangeQueryProcessor> changeQueryProcessorProvider;
 
-  private CheckerUuid checkerUuid;
-  private List<CheckState> states = new ArrayList<>(CheckState.values().length);
+  private String queryString;
 
   @Option(
-      name = "--checker",
-      metaVar = "UUID",
-      usage = "checker UUID formatted as '<scheme>:<id>'",
-      handler = CheckerUuidHandler.class)
-  public void setChecker(CheckerUuid checkerUuid) {
-    this.checkerUuid = checkerUuid;
-  }
-
-  @Option(name = "--state", metaVar = "STATE", usage = "check state")
-  public void addState(CheckState state) {
-    this.states.add(state);
+      name = "--query",
+      aliases = {"-q"},
+      metaVar = "QUERY",
+      usage = "check query")
+  public void setQuery(String queryString) {
+    this.queryString = queryString;
   }
 
   @Inject
   public ListPendingChecks(
+      CheckQueryBuilder checkQueryBuilder,
       Checkers checkers,
       Checks checks,
       RetryHelper retryHelper,
       Provider<ChangeQueryBuilder> queryBuilderProvider,
       Provider<ChangeQueryProcessor> changeQueryProcessorProvider) {
+    this.checkQueryBuilder = checkQueryBuilder;
     this.checkers = checkers;
     this.checks = checks;
     this.retryHelper = retryHelper;
@@ -85,77 +89,148 @@ public class ListPendingChecks implements RestReadView<TopLevelResource> {
   @Override
   public List<PendingChecksInfo> apply(TopLevelResource resource)
       throws RestApiException, IOException, ConfigInvalidException, OrmException {
-    if (states.isEmpty()) {
-      // If no state was specified, assume NOT_STARTED by default.
-      states.add(CheckState.NOT_STARTED);
+    if (queryString == null) {
+      throw new BadRequestException("query is required");
     }
 
-    if (checkerUuid == null) {
-      throw new BadRequestException("checker UUID is required");
+    Predicate<Check> query = validateQuery(parseQuery(queryString));
+    if (!hasStatePredicate(query)) {
+      query = Predicate.and(new CheckStatePredicate(CheckState.NOT_STARTED), query);
     }
 
-    Checker checker =
-        checkers
-            .getChecker(checkerUuid)
-            .orElseThrow(
-                () ->
-                    new UnprocessableEntityException(
-                        String.format("checker %s not found", checkerUuid)));
-
-    if (checker.getStatus() == CheckerStatus.DISABLED) {
+    Optional<Checker> checker = checkers.getChecker(getCheckerUuidFromQuery(query));
+    if (!checker.isPresent() || checker.get().isDisabled()) {
       return ImmutableList.of();
     }
 
     // The query system can only match against the current patch set; ignore non-current patch sets
     // for now.
     List<ChangeData> changes =
-        checker.queryMatchingChanges(
-            retryHelper, queryBuilderProvider.get(), changeQueryProcessorProvider);
+        checker
+            .get()
+            .queryMatchingChanges(
+                retryHelper, queryBuilderProvider.get(), changeQueryProcessorProvider);
+    CheckerUuid checkerUuid = checker.get().getUuid();
     List<PendingChecksInfo> pendingChecks = new ArrayList<>(changes.size());
     for (ChangeData cd : changes) {
-      getPostFilteredPendingChecks(cd.project(), cd.currentPatchSet().getId())
-          .ifPresent(pendingChecks::add);
+      PatchSet patchSet = cd.currentPatchSet();
+      CheckKey checkKey = CheckKey.create(cd.project(), patchSet.getId(), checkerUuid);
+
+      // Backfill if check is not present.
+      // Backfilling is only done for relevant checkers (checkers where the repository and the query
+      // matches the change). Since the change was found by executing the query of the checker we
+      // know that the checker is relevant for this patch set and hence backfilling should be done.
+      Check check =
+          checks
+              .getCheck(checkKey, GetCheckOptions.defaults())
+              .orElseGet(() -> Check.newBackfilledCheck(cd.project(), patchSet, checker.get()));
+
+      if (query.asMatchable().match(check)) {
+        pendingChecks.add(createPendingChecksInfo(cd.project(), patchSet, checkerUuid, check));
+      }
     }
     return pendingChecks;
   }
 
-  private Optional<PendingChecksInfo> getPostFilteredPendingChecks(
-      Project.NameKey repositoryName, PatchSet.Id patchSetId) throws OrmException, IOException {
-    CheckState checkState = getCheckState(repositoryName, patchSetId);
-    if (!states.contains(checkState)) {
-      return Optional.empty();
+  private Predicate<Check> parseQuery(String query) throws BadRequestException {
+    try {
+      return checkQueryBuilder.parse(query.trim());
+    } catch (QueryParseException e) {
+      throw new BadRequestException(e.getMessage());
     }
-    return Optional.of(
-        createPendingChecksInfo(repositoryName, patchSetId, checkerUuid, checkState));
   }
 
-  private CheckState getCheckState(Project.NameKey project, PatchSet.Id patchSetId)
-      throws OrmException, IOException {
-    Optional<Check> check =
-        checks.getCheck(
-            CheckKey.create(project, patchSetId, checkerUuid), GetCheckOptions.defaults());
+  private static Predicate<Check> validateQuery(Predicate<Check> predicate)
+      throws BadRequestException {
+    if (countCheckerPredicates(predicate) != 1)
+      throw new BadRequestException(
+          String.format(
+              "query must contain exactly 1 '%s' operator", CheckQueryBuilder.FIELD_CHECKER));
 
-    // Backfill if check is not present.
-    // Backfilling is only done for relevant checkers (checkers where the repository and the query
-    // matches the change). Since the change was found by executing the query of the checker we know
-    // that the checker is relevant for this patch set and hence backfilling should be done.
-    return check.map(Check::state).orElse(CheckState.NOT_STARTED);
+    // the root predicate must either be an AndPredicate ....
+    if (predicate instanceof AndPredicate) {
+      // if the root predicate is an AndPredicate, any of its direct children must be a
+      // CheckerPredicate, the other child predicates can be anything (including any combination of
+      // AndPredicate, OrPredicate and NotPredicate).
+      if (!predicate.getChildren().stream().anyMatch(CheckerPredicate.class::isInstance)) {
+        throw new BadRequestException(
+            String.format(
+                "query must be '%s:<checker-uuid>' or '%s:<checker-uuid> AND <other-operators>'",
+                CheckQueryBuilder.FIELD_CHECKER, CheckQueryBuilder.FIELD_CHECKER));
+      }
+      // ... or a CheckerPredicate
+    } else if (!(predicate instanceof CheckerPredicate)) {
+      throw new BadRequestException(
+          String.format(
+              "query must be '%s:<checker-uuid>' or '%s:<checker-uuid> AND <other-operators>'",
+              CheckQueryBuilder.FIELD_CHECKER, CheckQueryBuilder.FIELD_CHECKER));
+    }
+    return predicate;
+  }
+
+  private static boolean hasStatePredicate(Predicate<Check> predicate) {
+    if (predicate instanceof CheckStatePredicate) {
+      return true;
+    }
+    if (predicate.getChildCount() == 0) {
+      return false;
+    }
+    return predicate.getChildren().stream().anyMatch(ListPendingChecks::hasStatePredicate);
+  }
+
+  /**
+   * Counts the number of {@link CheckerPredicate}s in the given predicate.
+   *
+   * <p>This method doesn't validate that the checker predicates appear in any particular location.
+   *
+   * @param predicate the predicate in which the checker predicates should be counted
+   * @return the number of checker predicates in the given predicate
+   */
+  private static int countCheckerPredicates(Predicate<Check> predicate) {
+    if (predicate instanceof CheckerPredicate) {
+      return 1;
+    }
+    if (predicate.getChildCount() == 0) {
+      return 0;
+    }
+    return predicate.getChildren().stream()
+        .mapToInt(ListPendingChecks::countCheckerPredicates)
+        .sum();
+  }
+
+  private static CheckerUuid getCheckerUuidFromQuery(Predicate<Check> predicate) {
+    // the query validation (see #validateQuery(Predicate<Check>)) ensures that there is exactly 1
+    // CheckerPredicate and that it is on the first or second level of the predicate tree.
+
+    if (predicate instanceof CheckerPredicate) {
+      return ((CheckerPredicate) predicate).getCheckerUuid();
+    }
+
+    checkState(predicate.getChildCount() > 0, "no checker predicate found: %s", predicate);
+    Optional<CheckerPredicate> checkerPredicate =
+        predicate.getChildren().stream()
+            .filter(CheckerPredicate.class::isInstance)
+            .map(p -> (CheckerPredicate) p)
+            .findAny();
+    return checkerPredicate
+        .map(CheckerPredicate::getCheckerUuid)
+        .orElseThrow(
+            () ->
+                new IllegalStateException(
+                    String.format("no checker predicate found: %s", predicate)));
   }
 
   private static PendingChecksInfo createPendingChecksInfo(
-      Project.NameKey repositoryName,
-      PatchSet.Id patchSetId,
-      CheckerUuid checkerUuid,
-      CheckState checkState) {
+      Project.NameKey repositoryName, PatchSet patchSet, CheckerUuid checkerUuid, Check check) {
     PendingChecksInfo pendingChecksInfo = new PendingChecksInfo();
 
     pendingChecksInfo.patchSet = new CheckablePatchSetInfo();
     pendingChecksInfo.patchSet.repository = repositoryName.get();
-    pendingChecksInfo.patchSet.changeNumber = patchSetId.getParentKey().get();
-    pendingChecksInfo.patchSet.patchSetId = patchSetId.get();
+    pendingChecksInfo.patchSet.changeNumber = patchSet.getId().getParentKey().get();
+    pendingChecksInfo.patchSet.patchSetId = patchSet.getPatchSetId();
 
     pendingChecksInfo.pendingChecks =
-        ImmutableMap.of(checkerUuid.get(), new PendingCheckInfo(checkState));
+        ImmutableMap.of(checkerUuid.get(), new PendingCheckInfo(check.state()));
 
     return pendingChecksInfo;
   }

java/com/google/gerrit/plugins/checks/index/CheckerPredicate.java
@@ -1,0 +1,47 @@
+// Copyright (C) 2019 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.plugins.checks.index;
+
+import static java.util.Objects.requireNonNull;
+
+import com.google.gerrit.index.query.QueryParseException;
+import com.google.gerrit.plugins.checks.Check;
+import com.google.gerrit.plugins.checks.CheckerUuid;
+import com.google.gwtorm.server.OrmException;
+
+public class CheckerPredicate extends CheckPredicate {
+  public static CheckerPredicate parse(String value) throws QueryParseException {
+    return new CheckerPredicate(
+        CheckerUuid.tryParse(value)
+            .orElseThrow(
+                () -> new QueryParseException(String.format("invalid checker UUID: %s", value))));
+  }
+
+  private final CheckerUuid checkerUuid;
+
+  public CheckerPredicate(CheckerUuid checkerUuid) {
+    super(CheckQueryBuilder.FIELD_CHECKER, checkerUuid.toString());
+    this.checkerUuid = requireNonNull(checkerUuid, "checkerUuid");
+  }
+
+  @Override
+  public boolean match(Check check) throws OrmException {
+    return checkerUuid.equals(check.key().checkerUuid());
+  }
+
+  public CheckerUuid getCheckerUuid() {
+    return checkerUuid;
+  }
+}