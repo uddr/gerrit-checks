0302ebcf193ed25266c03024698d8418c4620bee
Dhruv Srivastava <dhruvsri@google.com>
2019-07-16 09:32:09 +0000

    Revert "Stop polling for checks if tab goes out of focus"
    This reverts commit 030064edb8bc1661e1992fe68def36368f588798.
    
    Reason for revert: <memory leaks as listener was not unsubscribed>
    
    Change-Id: I2dbf811d53a7f02a4bfef5b7201fcec507d708a3

--- gr-checks/gr-checks-chip-view.js
+++ gr-checks/gr-checks-chip-view.js
@@ -84,8 +84,7 @@
         type: Boolean,
         value: false
       },
-      pollChecksInterval: Object,
-      visibilityChangeListener: Object
+      pollChecksInterval: Object
     },
 
     observers: [
@@ -125,27 +124,13 @@
       });
     },
 
-    onVisibililityChange() {
-      if (document.hidden) {
-        clearInterval(this.pollChecksInterval);
-        return;
-      }
-      this._pollChecksRegularly(this.change, this.revision, this.getChecks);
-    },
-
     _pollChecksRegularly(change, revision, getChecks) {
       if (this.pollChecksInterval) {
         clearInterval(this.pollChecksInterval);
       }
       const poll = () => this._fetchChecks(change, revision, getChecks);
       poll();
-      this.pollChecksInterval = setInterval(poll, CHECKS_POLL_INTERVAL_MS);
-      if (!this.visibilityChangeListener) {
-        this.visibilityChangeListener = document.addEventListener(
-          'visibilitychange',
-          this.onVisibililityChange.bind(this)
-        );
-      }
+      this.pollChecksInterval = setInterval(poll, CHECKS_POLL_INTERVAL_MS)
     },
 
     /**
