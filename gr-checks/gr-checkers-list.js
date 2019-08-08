ce3c7d0e5013d7653bd96032f62f6724f19d6644
Dhruv Srivastava <dhruvsri@google.com>
2019-08-08 16:35:36 +0200

    Add UI for adding and editing checkers
    * Add a configure button in the checks-view table
    * Show a list of existing checkers upon clicking configure
    * Allow user to create checkers
    * Allow user to edit existing checkers
    
    Change-Id: I779aaaf488645f3db56c942496c78c2b7c7ad185

---
+++ gr-checks/gr-checkers-list.js
@@ -0,0 +1,174 @@
+(function() {
+  'use strict';
+  const CHECKERS_PER_PAGE = 15;
+  const GET_CHECKERS_URL = "/a/plugins/checks/checkers/";
+
+  /**
+   * Show a list of all checkers along with creating/editing them
+   */
+  Polymer({
+    is: 'gr-checkers-list',
+    properties: {
+      pluginRestApi: Object,
+      // Checker that will be passed to the editOverlay modal
+      checker: Object,
+      _checkers: Array,
+      // List of checkers that contain the filtered text
+      _filteredCheckers: Array,
+      _loading: {
+        type: Boolean,
+        value: true,
+      },
+      _filter: {
+        type: String,
+        value: '',
+      },
+      _visibleCheckers: {
+        type: Array,
+        computed: '_computeVisibleCheckers(_startingIndex, _filteredCheckers)',
+        observer: '_visibleCheckersChanged'
+      },
+      _createNewCapability: {
+        type: Boolean,
+        value: true,
+      },
+      _startingIndex: {
+        type: Number,
+        value: 0
+      },
+      _showNextButton: {
+        type: Boolean,
+        value: true,
+        computed: '_computeShowNextButton(_startingIndex, _filteredCheckers)'
+      },
+      _showPrevButton: {
+        type: Boolean,
+        value: true,
+        computed: '_computeShowPrevButton(_startingIndex, _filteredCheckers)'
+      },
+    },
+    observers: [
+      '_showCheckers(_checkers, _filter)',
+    ],
+
+    attached() {
+      this._getCheckers();
+    },
+
+    _contains(target, keyword) {
+      return target.toLowerCase().includes(keyword.toLowerCase().trim());
+    },
+
+    _visibleCheckersChanged(currentVisibleCheckers, previousVisibleCheckers) {
+      if (!currentVisibleCheckers || !previousVisibleCheckers) {
+        return;
+      }
+      if (currentVisibleCheckers.length !== previousVisibleCheckers.length) {
+        this.fire('resize', {bubbles: false});
+      }
+    },
+
+    _showCheckers(_checkers, _filter) {
+      if (!_checkers) return;
+      if (!_filter) _filter = '';
+      // TODO(dhruvsri): highlight matching part
+      this._filteredCheckers = this._checkers.filter(checker =>
+        this._contains(checker.name, this._filter) ||
+        this._contains(checker.repository, this._filter))
+      this._startingIndex = 0;
+    },
+
+    computeLoadingClass(loading) {
+      return loading ? 'loading' : '';
+    },
+
+    _computeVisibleCheckers(_startingIndex, _filteredCheckers) {
+      if (!_filteredCheckers) {
+        return [];
+      }
+      return this._filteredCheckers.slice(this._startingIndex,
+        this._startingIndex + CHECKERS_PER_PAGE);
+    },
+
+    _computeShowNextButton(_startingIndex, _filteredCheckers) {
+      if (!_filteredCheckers) {
+        return false;
+      }
+      return _startingIndex + CHECKERS_PER_PAGE < _filteredCheckers.length;
+    },
+
+    _computeShowPrevButton(_startingIndex, _filteredCheckers) {
+      if (!_filteredCheckers) {
+        return false;
+      }
+      return _startingIndex >= CHECKERS_PER_PAGE;
+    },
+
+    _handleNextClicked() {
+      if (this._startingIndex + CHECKERS_PER_PAGE <
+        this._filteredCheckers.length) {
+          this._startingIndex += CHECKERS_PER_PAGE;
+      }
+    },
+
+    _handlePrevClicked() {
+      if (this._startingIndex >= CHECKERS_PER_PAGE) {
+        this._startingIndex -= CHECKERS_PER_PAGE;
+      }
+    },
+
+    _getCheckers() {
+      this.pluginRestApi.fetchJSON({
+        method: 'GET',
+        url: GET_CHECKERS_URL,
+      }).then(checkers => {
+        if (!checkers) { return; }
+        this._checkers = checkers;
+        this._startingIndex = 0;
+        this._loading = false;
+      });
+    },
+
+    _handleEditConfirm() {
+      this.$.editModal.handleEditChecker();
+    },
+
+    _handleEditIconClicked(e) {
+      let checker = e.model.item;
+      this.checker = checker;
+      this.$.editOverlay.open();
+    },
+
+    _handleEditCancel(e) {
+      if (e.detail.reload) {
+        this._getCheckers();
+      }
+      this.$.editOverlay.close();
+    },
+
+    _computeCreateClass(createNew) {
+      return createNew ? 'show' : '';
+    },
+
+    _computeBlocking(checker) {
+      return (checker && checker.blocking && checker.blocking.length > 0)
+        ? "YES": "NO";
+    },
+
+    _handleCreateConfirm() {
+      this.$.createNewModal.handleCreateChecker();
+    },
+
+    _handleCreateClicked() {
+      this.$.createOverlay.open();
+    },
+
+    _handleCreateCancel(e) {
+      if (e.detail.reload) {
+        this._getCheckers();
+      }
+      this.$.createOverlay.close();
+    },
+
+  })
+})();
---
+++ gr-checks/gr-create-checkers-dialog.js
@@ -0,0 +1,258 @@
+(function() {
+  'use strict';
+
+  const REPOS_PER_PAGE = 6;
+  const CREATE_CHECKER_URL = '/plugins/checks/checkers/';
+  const SCHEME_PATTERN = /^[\w-_.]*$/;
+
+  Polymer({
+    is: 'gr-create-checkers-dialog',
+    _legacyUndefinedCheck: true,
+
+    properties: {
+      checker: {
+        type: Object,
+        observer: '_checkerChanged'
+      },
+      _name: String,
+      _scheme: String,
+      _id: String,
+      _uuid: {
+        type: String,
+        value: ""
+      },
+      pluginRestApi: Object,
+      _url: String,
+      _description: String,
+      _getRepoSuggestions: {
+        type: Function,
+        value() {
+          return this._repoSuggestions.bind(this)
+        }
+      },
+      // The backend might support multiple repos in the future
+      // which is why I decided to keep it as an array.
+      _repos: {
+        type: Array,
+        value: [],
+        notify: true,
+      },
+      _repositorySelected: {
+        type: Boolean,
+        value: false
+      },
+      _handleOnRemove: Function,
+      _errorMsg: {
+        type: String,
+        value: ''
+      },
+      _statuses: {
+        type: Array,
+        value: [
+          {
+            "text": "ENABLED",
+            "value": "ENABLED"
+          },
+          {
+            "text": "DISABLED",
+            "value": "DISABLED"
+          }
+        ],
+        readOnly: true
+      },
+      _required: {
+        type: Boolean,
+        value: false
+      },
+      _status: String,
+      _edit: {
+        type: Boolean,
+        value: false
+      },
+      _query: String
+    },
+
+    behaviours: [
+      Gerrit.FireBehavior,
+    ],
+    /**
+    * Fired when the cancel button is pressed.
+    *
+    * @event cancel
+    */
+
+
+    observers: [
+      '_updateUUID(_scheme, _id)',
+    ],
+
+    _checkerChanged() {
+      if (!this.checker) {
+        console.warn("checker not set");
+        return;
+      }
+      this._edit = true;
+      this._scheme = this.checker.uuid.split(':')[0];
+      this._id = this.checker.uuid.split(':')[1];
+      this._name = this.checker.name;
+      this._description = this.checker.description || '';
+      this._url = this.checker.url || '';
+      this._query = this.checker.query || '';
+      this._required = this.checker.blocking &&
+                        this.checker.blocking.length > 0;
+      if (this.checker.repository) {
+        this._repositorySelected = true;
+        this.set('_repos', [{name: this.checker.repository}]);
+      }
+      this._status = this.checker.status;
+    },
+
+    _updateUUID(_scheme, _id) {
+      this._uuid = _scheme + ":" + _id;
+    },
+
+    _handleStatusChange(e) {
+      this._status = e.detail.value;
+    },
+
+    _validateRequest() {
+      if (!this._name) {
+        this._errorMsg = 'Name cannot be empty';
+        return false;
+      }
+      if (this._description && this._description.length > 1000) {
+        this._errorMsg = 'Description should be less than 1000 characters';
+        return false;
+      }
+      if (!this._repositorySelected) {
+        this._errorMsg = 'Select a repository';
+        return false;
+      }
+      if (!this._scheme) {
+        this._errorMsg = 'Scheme cannot be empty.';
+        return false;
+      }
+      if (this._scheme.match(SCHEME_PATTERN) == null) {
+        this._errorMsg =
+          'Scheme must contain [A-Z], [a-z], [0-9] or {"-" , "_" , "."}';
+        return false;
+      }
+      if (this._scheme.length > 100) {
+        this._errorMsg = 'Scheme must be shorter than 100 characters';
+        return false;
+      }
+      if (!this._id) {
+        this._errorMsg = 'ID cannot be empty.';
+        return false;
+      }
+      if (this._id.match(SCHEME_PATTERN) == null) {
+        this._errorMsg =
+          'ID must contain [A-Z], [a-z], [0-9] or {"-" , "_" , "."}';
+        return false;
+      }
+      return true;
+    },
+
+    // TODO(dhruvsri): make sure dialog is scrollable.
+
+    _createChecker(checker) {
+      return this.pluginRestApi.send(
+        'POST',
+        CREATE_CHECKER_URL,
+        checker,
+      )
+    },
+
+    _editChecker(checker) {
+      const url = CREATE_CHECKER_URL + checker.uuid;
+      return this.pluginRestApi.send(
+        'POST',
+        url,
+        checker
+      )
+    },
+
+    handleEditChecker() {
+      if (!this._validateRequest()) return;
+      this._editChecker(this._getCheckerRequestObject()).then(
+        res => {
+          if (res) {
+            this._errorMsg = '';
+            this.fire('cancel', {reload: true}, {bubbles: true});
+          }
+        },
+        error => {
+          this._errorMsg = error;
+        }
+      )
+    },
+
+    _getCheckerRequestObject() {
+      return {
+        "name" : this._name,
+        "description" : this._description || '',
+        "uuid" : this._uuid,
+        "repository": this._repos[0].name,
+        "url" : this._url,
+        "status": this._status,
+        "blocking": this._required ? ["STATE_NOT_PASSING"] : [],
+        "query": this._query
+      }
+    },
+
+    handleCreateChecker() {
+      if (!this._validateRequest()) return;
+      // Currently after creating checker there is no reload happening (as
+      // this would result in the user exiting the screen).
+      this._createChecker(this._getCheckerRequestObject()).then(
+        res => {
+          if (res) this._cleanUp();
+        },
+        error => {
+          this._errorMsg = error;
+        }
+      )
+    },
+
+    _cleanUp() {
+      this._name = '';
+      this._scheme = '';
+      this._id = '';
+      this._uuid = '';
+      this._description = '';
+      this._repos = [];
+      this._repositorySelected = false;
+      this._errorMsg = '';
+      this._required = false;
+      this._query = '';
+      this._status = '';
+      this.fire('cancel', {reload: true}, {bubbles: true});
+    },
+
+    _repoSuggestions(filter) {
+      const _makeSuggestion = repo => {return {name: repo.name, value: repo}};
+      return this.pluginRestApi.getRepos(filter, REPOS_PER_PAGE).then(
+        repos => repos.map(repo =>  _makeSuggestion(repo))
+      )
+    },
+
+    _handleRepositorySelected(e) {
+      this.push('_repos', e.detail.value);
+      this._repositorySelected = true;
+    },
+
+    _handleRequiredCheckBoxClicked() {
+      this._required = !this._required;
+    },
+
+    _handleOnRemove(e) {
+      let idx = this._repos.indexOf(e.detail.repo);
+      if (idx == -1) return;
+      this.splice('_repos', idx, 1);
+      if (this._repos.length == 0) {
+        this._repositorySelected = false;
+      }
+    },
+
+  });
+})();
