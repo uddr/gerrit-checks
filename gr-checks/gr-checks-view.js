GerritCodeReview/plugins_checks

Limit displayed checks to 20 and add Show All checks button.
Change-Id: I5e6718ec5072e1de04f4b543469ceac44819629e

gr-checks/gr-checks-view.js
@@ -17,6 +17,7 @@
   const CheckStateFilters = [STATE_ALL, ...StatusPriorityOrder];
 
   const CHECKS_POLL_INTERVAL_MS = 60 * 1000;
+  const CHECKS_LIMIT = 20;
 
   /**
    * @typedef {{
@@ -61,7 +62,7 @@
       _visibleChecks: {
         type: Array,
         computed: '_computeVisibleChecks(_checks, _currentStatus, ' +
-          '_showBlockingChecksOnly)',
+          '_showBlockingChecksOnly, _showAllChecks)',
       },
       _statuses: Array,
       pollChecksInterval: Number,
@@ -89,6 +90,16 @@
         type: Boolean,
         value: false,
       },
+      _showAllChecks: {
+        type: Boolean,
+        value: false,
+      },
+      _filteredChecks: Array,
+      _showMoreChecksButton: {
+        type: Boolean,
+        value: false,
+        notify: true,
+      },
     },
 
     observers: [
@@ -111,6 +122,10 @@
       this.unlisten(document, 'visibilitychange', '_onVisibililityChange');
     },
 
+    _toggleShowChecks() {
+      this._showAllChecks = !this._showAllChecks;
+    },
+
     _computePatchSetDropdownItems(change) {
       return Object.values(change.revisions)
           .filter(patch => patch._number !== 'edit')
@@ -123,13 +138,24 @@
           .sort((a, b) => b.value - a.value);
     },
 
-    _computeVisibleChecks(checks, status, showBlockingChecksOnly) {
+    _computeShowText(showAllChecks) {
+      return showAllChecks ? 'Show Less' : 'Show All';
+    },
+
+    _computeVisibleChecks(checks, status, showBlockingChecksOnly,
+        showAllChecks) {
       if (!checks) return [];
-      return checks.filter(check => {
+      this._filteredChecks = checks.filter(check => {
         if (showBlockingChecksOnly && (!check.blocking ||
             !check.blocking.length)) return false;
         return status === STATE_ALL || check.state === status;
       });
+      if (this._filteredChecks.length <= CHECKS_LIMIT) {
+        return this._filteredChecks;
+      }
+      this._showMoreChecksButton = true;
+      return showAllChecks ? this._filteredChecks : this._filteredChecks.slice(
+          0, CHECKS_LIMIT);
     },
 
     _handleRevisionUpdate(revision) {
@@ -243,6 +269,12 @@
       getChecks(change._number, revisionNumber).then(checks => {
         if (revisionNumber !== this._currentPatchSet) return;
         if (checks && checks.length) {
+          // REMOVE
+          const xc = [...checks];
+          for (let i = 1; i <= 10; i++) {
+            checks.push(...xc);
+          }
+          // REMOVE
           checks.sort((a, b) => this._orderChecks(a, b));
           if (!this._checks) {
             this._checks = checks;

uddr/gerrit-checks

gr-checks/gr-checks-view.js
@@ -17,6 +17,7 @@
   const CheckStateFilters = [STATE_ALL, ...StatusPriorityOrder];
 
   const CHECKS_POLL_INTERVAL_MS = 60 * 1000;
+  const CHECKS_LIMIT = 20;
 
   /**
    * @typedef {{
@@ -87,12 +88,22 @@
         type: Boolean,
         value: false,
       },
+      _showAllChecks: {
+        type: Boolean,
+        value: false,
+      },
+      _filteredChecks: Array,
+      _showMoreChecksButton: {
+        type: Boolean,
+        value: false,
+        notify: true,
+      },
     },
 
     observers: [
       '_pollChecksRegularly(change, _currentPatchSet, getChecks)',
       '_updateVisibleChecks(_checks.*, _currentStatus, ' +
-          '_showBlockingChecksOnly)',
+          '_showBlockingChecksOnly, _showAllChecks)',
     ],
 
     attached() {
@@ -111,6 +122,10 @@
       this.unlisten(document, 'visibilitychange', '_onVisibililityChange');
     },
 
+    _toggleShowChecks() {
+      this._showAllChecks = !this._showAllChecks;
+    },
+
     _computePatchSetDropdownItems(change) {
       return Object.values(change.revisions)
           .filter(patch => patch._number !== 'edit')
@@ -123,10 +138,15 @@
           .sort((a, b) => b.value - a.value);
     },
 
-    _updateVisibleChecks(checksRecord, status, showBlockingChecksOnly) {
+    _computeShowText(showAllChecks) {
+      return showAllChecks ? 'Show Less' : 'Show All';
+    },
+
+    _updateVisibleChecks(checksRecord, status, showBlockingChecksOnly,
+        showAllChecks) {
       const checks = checksRecord.base;
       if (!checks) return [];
-      this._visibleChecks = checks.filter(check => {
+      this._filteredChecks = checks.filter(check => {
         if (showBlockingChecksOnly && (!check.blocking ||
             !check.blocking.length)) return false;
         return status === STATE_ALL || check.state === status;
@@ -137,6 +157,9 @@
       If not notified, then the message for the check is not displayed after
       clicking the toggle
       */
+      this._showMoreChecksButton = this._filteredChecks.length > CHECKS_LIMIT;
+      this._visibleChecks = this._filteredChecks.slice(0, showAllChecks ?
+        undefined : CHECKS_LIMIT);
       this._visibleChecks.forEach((val, idx) =>
         this.notifyPath(`_visibleChecks.${idx}.showCheckMessage`));
     },