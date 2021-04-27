b349da477cf472e55184f6d24f96a6c8279fc6c0
Ben Rohlfs <brohlfs@google.com>
2021-04-27 13:51:06 +0200

    Remove old obsolete Checks UI
    Keep the dialog for checker configuration. Will add this as a top-level
    action to the new Checks UI.
    
    Change-Id: If77cb2615ba9a0838a39f478aa225df8f608a074

--- gr-checks/gr-checks.js
+++ gr-checks/gr-checks.js
@@ -14,65 +14,12 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import './gr-checks-chip-view.js';
-import './gr-checks-view.js';
-import './gr-checks-change-view-tab-header-view.js';
-import './gr-checks-change-list-header-view.js';
-import './gr-checks-change-list-item-cell-view.js';
-import './gr-checks-item.js';
-import './gr-checks-status.js';
 import {RebootFetcher} from './gr-checks-reboot.js';
 
-function installChecksLegacy(plugin) {
-  const getChecks = (change, revision) => {
-    return plugin.restApi().get(
-        '/changes/' + change + '/revisions/' + revision + '/checks?o=CHECKER');
-  };
-
-  // TODO(brohlfs): Enable this dashboard column when search queries start
-  // returning checks.
-  // plugin.registerDynamicCustomComponent(
-  //     'change-list-header',
-  //     'gr-checks-change-list-header-viewgg');
-  // plugin.registerDynamicCustomComponent(
-  //     'change-list-item-cell',
-  //     'gr-checks-change-list-item-cell-view');
-  plugin.registerCustomComponent(
-      'commit-container',
-      'gr-checks-chip-view').onAttached(
-      view => {
-        view['getChecks'] = getChecks;
-      }
-  );
-  plugin.registerDynamicCustomComponent(
-      'change-view-tab-header',
-      'gr-checks-change-view-tab-header-view'
-  );
-  plugin.registerDynamicCustomComponent(
-      'change-view-tab-content',
-      'gr-checks-view').onAttached(
-      view => {
-        view['isConfigured'] = repository => Promise.resolve(true);
-        view['getChecks'] = getChecks;
-      });
-}
-
-function installChecksReboot(plugin) {
+Gerrit.install(plugin => {
   const checksApi = plugin.checks();
   const fetcher = new RebootFetcher(plugin.restApi());
   checksApi.register({
-    fetch: (data) => fetcher.fetch(data)
+    fetch: data => fetcher.fetch(data),
   });
-}
-
-Gerrit.install(plugin => {
-  const experiments = window.ENABLED_EXPERIMENTS || [];
-  if (experiments.includes("UiFeature__ci_reboot_checks_checks")) {
-    // Until end of 2020 this is only interesting for developing purposes. So
-    // no real user is affected for the time being.
-    console.log('Installing checks REBOOT plugin.');
-    installChecksReboot(plugin);
-  } else {
-    installChecksLegacy(plugin);
-  }
 });
